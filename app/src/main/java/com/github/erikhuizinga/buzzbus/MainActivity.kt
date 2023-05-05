3cd10f1db88989c8155ad36fa470e72ffdb252cf
Erik Huizinga <erikhuizinga@users.noreply.github.com>
2023-05-05 10:45:41 +0200

    Log s2s events to localhost

--- app/src/main/java/com/github/erikhuizinga/buzzbus/MainActivity.kt
+++ app/src/main/java/com/github/erikhuizinga/buzzbus/MainActivity.kt
@@ -11,7 +11,6 @@ import com.github.erikhuizinga.buzzbus.analytics.JitsuLogger
 import com.github.erikhuizinga.buzzbus.analytics.Logger
 import com.github.erikhuizinga.buzzbus.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,15 +18,15 @@ class MainActivity : AppCompatActivity() {
 
     private val logger: Logger by lazy {
         JitsuLogger.getInstance(
-            endpoint = URL("http://localhost:8000/"),
-            apiKey = "android.iluf8ek9uulm4pzmw8spf1.792sasq4vhc68a7rtk3hpq"
+            endpoint = "http://$LOCAL_HOST:8000/",
+            apiKey = "s2s.iluf8ek9uulm4pzmw8spf1.lpihxlbcu6pz5tdlygfkkd"
         )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        logger.logEvent("MainActivity.onCreate")
+        logger.logEvent("name" to "MainActivity.onCreate")
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -40,7 +39,10 @@ class MainActivity : AppCompatActivity() {
             val destination = navDestination.label
             Log.d("MainActivity", navDestination.toString())
             Log.d("MainActivity", "Destination changed to $destination")
-            logger.logEvent("MainActivity.onDestinationChanged", "destination" to destination)
+            logger.logEvent(
+                "name" to "MainActivity.onDestinationChanged",
+                "destination" to destination,
+            )
         }
 
         // Passing each menu ID as a set of Ids because each
@@ -52,5 +54,13 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private companion object {
+        /**
+         * 10.0.2.2 is the Android emulator's alias to localhost, see the docs on
+         * [Network address space](https://developer.android.com/studio/run/emulator-networking.html#networkaddresses).
+         */
+        private const val LOCAL_HOST = "10.0.2.2"
     }
 }
--- app/src/main/java/com/github/erikhuizinga/buzzbus/analytics/Logger.kt
+++ app/src/main/java/com/github/erikhuizinga/buzzbus/analytics/Logger.kt
@@ -3,46 +3,52 @@ package com.github.erikhuizinga.buzzbus.analytics
 import android.util.Log
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
-import io.ktor.client.request.get
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import java.io.Closeable
-import java.net.URL
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
+import org.json.JSONObject
 
 interface Logger {
-    fun logEvent(name: String, vararg params: Pair<String, Any?>)
-    fun logEvent(name: String, params: Map<String, Any?>)
+    fun logEvent(vararg params: Pair<String, Any?>)
+    fun logEvent(params: Map<String, Any?>)
 }
 
-class JitsuLogger private constructor(endpoint: URL, apiKey: String) : Logger, Closeable {
-    private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
-    private val client = HttpClient(CIO)
+class JitsuLogger private constructor(
+    endpoint: String,
+    private val apiKey: String,
+) : Logger, Closeable {
+    private val client = HttpClient(CIO) {
+        defaultRequest {
+            contentType(ContentType.Application.Json)
+            header("X-Auth-Token", apiKey)
+        }
+    }
 
-    override fun logEvent(name: String, vararg params: Pair<String, Any?>) =
-        logEvent(name, params.toMap())
+    private val eventsEndpoint = "${endpoint.dropLastWhile { it == '/' }}/api/v1/s2s/event"
 
-    override fun logEvent(name: String, params: Map<String, Any?>) {
-        coroutineScope.launch {
-            val response = client.get("https://ktor.io/")
-            Log.d("JitsuLogger", response.status.toString())
+    override fun logEvent(params: Map<String, Any?>) {
+        client.launch {
+            val response = client.post(eventsEndpoint) {
+                setBody((JSONObject.wrap(params) ?: null).toString())
+            }
             Log.d("JitsuLogger", response.toString())
         }
     }
 
-    override fun close() {
-        coroutineScope.cancel()
-        client.close()
-    }
+    override fun logEvent(vararg params: Pair<String, Any?>) = logEvent(params.toMap())
+    override fun close() = client.close()
 
     companion object {
         private var apiKey: String? = null
-        private var endpoint: URL? = null
+        private var endpoint: String? = null
         private var instance: JitsuLogger? = null
 
-        fun getInstance(endpoint: URL, apiKey: String): Logger {
+        fun getInstance(endpoint: String, apiKey: String): Logger {
             val currentInstance = instance
             return if (currentInstance == null || apiKey != this.apiKey || endpoint != this.endpoint) {
                 currentInstance?.close()
